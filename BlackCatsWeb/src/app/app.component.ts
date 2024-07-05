065cb9e16da8416dfbf265ad8da8bf7bfd7fa47c
Hidayat Bhatt <hidayatbhatt@gmail.com>
2024-07-05 22:59:19 +0530

    minor changes

--- BlackCatsWeb/src/app/app.component.ts
+++ BlackCatsWeb/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavComponent } from './nav/nav.component';
 
 @Component({
   selector: 'app-root',
