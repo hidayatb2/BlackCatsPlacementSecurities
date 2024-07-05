e259823cbdab3b7b80d696dc606f72f45b5a9ec8
himayatb3 <himayatb3@gmail.com>
2024-07-05 21:22:21 +0530

    Architecture

---
+++ BlackCatsAPI/BlackCats_Persistance/AssemblyRefrence.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BlackCats_Persistance
+{
+    public static class AssemblyRefrence
+    {
+        public static IServiceCollection AddPersistanceService(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+        }
+
+    }
+}
--- BlackCatsWeb/src/app/app.component.ts
+++ BlackCatsWeb/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from './nav/nav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+<<<<<<< Updated upstream
   imports: [RouterOutlet],
+=======
+  imports: [RouterOutlet, NavComponent],
+>>>>>>> Stashed changes
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
