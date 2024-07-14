788f519588074672544a3782ebedefed59f0b74a
himayatb3 <himayatb3@gmail.com>
2024-07-14 17:04:25 +0530

    Repo Pattern

---
+++ BlackCatsAPI/BlackCats_Domain/Entities/User.cs
@@ -0,0 +1,26 @@
+﻿using BlackCats_Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackCats_Domain.Entities
+{
+    public class User:BaseEntity
+    {
+        public string UserName { get; set; } =string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string Name { get; set; } = string.Empty;
+
+        public int ContactNo { get; set; } 
+
+        public string Password { get; set; } = string.Empty;
+
+        public string RefreshToken { get; set; } = string.Empty;
+
+        public UserRole UserRole { get; set; }
+    }
+}
---
+++ BlackCatsAPI/BlackCats_Persistance/AssemblyRefrence.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace BlackCats_Persistance
+{
+    public static class AssemblyRefrence
+    {
+        public static IServiceCollection AddPersistanceService(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<BCPSDbContext>(options => options.UseMySql(configuration.GetConnectionString(nameof(BCPSDbContext)), ServerVersion.AutoDetect(configuration.GetConnectionString(nameof(BCPSDbContext)))));
+            return services;
+        }
+
+    }
+}
