810ded5dbb1ba796fc70a8bc971eb564274aad19
zubair-shabir <zubairshabir980@gmail.com>
2024-08-06 23:32:40 +0530

    completed home page Ui

--- BlackCatsWeb/src/app/user/components/nav/nav.component.ts
+++ BlackCatsWeb/src/app/user/components/nav/nav.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+import {
+  aboutUsData,
+
+} from '../../fakeData';
+
 @Component({
   selector: 'bcss-nav',
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss'
 })
 export class NavComponent {
+  About: any = [];
 
+
+  ngOnInit(){
+    this.getAboutUsData()
+  }
+
+  getAboutUsData() {
+    this.About = aboutUsData;
+    console.log(this.About);
+  }
 }
---
+++ BlackCatsWeb/src/app/user/fakeData.ts
@@ -0,0 +1,67 @@
+export const aboutUsData = {
+  logo: 'https://flowbite.com/docs/images/logo.svg',
+  companyName: ' Black Security Solutions',
+  about:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum iste voluptate necessitatibus. Dignissimos quaerat sit aut quod debitis sunt beatae dolorem exercitationem dicta nemo, voluptatem qui corporis, non aspernatur similique atque earum omnis dolorum velit! Corporis assumenda earum qui pariatur beatae suscipit fuga iure consectetur tempora, exercitationem cum. Earum fuga perspiciatis nulla mollitia aperiam placeat eveniet at quia laboriosam architecto delectus perferendis ipsum ullam illum nostrum voluptas, illo, reiciendis sit odio ipsa nemo dignissimos! Perspiciatis sunt voluptatum totam similique soluta saepe dolor doloremque sed dicta earum quasi iusto laboriosam esse nemo natus hic quod ipsa nam magni, facilis nihil numquam. Hic maxime, quasi autem a eaque blanditiis laboriosam magnam aliquam sint rem repellat illo officia molestiae tenetur expedita consectetur, voluptates quo eum rerum! Natus, debitis aspernatur! Quibusdam nostrum similique a, dolor sit quasi quos, eum quisquam nesciunt porro pariatur suscipit illum veritatis, consectetur doloribus maiores veniam voluptates aut at est. Neque, eaque distinctio unde vel, corrupti eveniet voluptate asperiores iure, culpa voluptas exercitationem quidem doloribus quo dolores earum nam tempora ipsam? Provident hic tempora voluptatibus. Repellat, omnis eveniet? Laboriosam sapiente aperiam repudiandae. Quasi, illum iure fugiat, harum facere explicabo similique sequi amet perspiciatis quos tempora blanditiis delectus? Debitis, nesciunt repudiandae.',
+};
+
+export const CoursalData = [
+  'https://images.pexels.com/photos/1119152/pexels-photo-1119152.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  'https://images.pexels.com/photos/270220/pexels-photo-270220.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  'https://images.pexels.com/photos/2375264/pexels-photo-2375264.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  'https://images.pexels.com/photos/1746207/pexels-photo-1746207.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  'https://images.pexels.com/photos/270220/pexels-photo-270220.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+];
+
+export const AchievmentImagesData = [
+  'https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg',
+  'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg',
+  'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg',
+  'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg',
+  'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg',
+  'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg',
+  'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg',
+  'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg',
+  'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg',
+  'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg',
+  'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg',
+  'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg',
+];
+
+export const ServicesData = [
+  {
+    image:
+      'https://images.pexels.com/photos/8591606/pexels-photo-8591606.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    title: 'Noteworthy technology acquisitions 2021',
+    description:
+      'Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.',
+  },
+  {
+    image:
+      'https://images.pexels.com/photos/8591606/pexels-photo-8591606.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    title: 'Noteworthy technology acquisitions 2021',
+    description:
+      'Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.',
+  },
+  {
+    image:
+      'https://images.pexels.com/photos/8591606/pexels-photo-8591606.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    title: 'Noteworthy technology acquisitions 2021',
+    description:
+      'Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.',
+  },
+  {
+    image:
+      'https://images.pexels.com/photos/8591606/pexels-photo-8591606.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    title: 'Noteworthy technology acquisitions 2021',
+    description:
+      'Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.',
+  },
+  {
+    image:
+      'https://images.pexels.com/photos/8591606/pexels-photo-8591606.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    title: 'Noteworthy technology acquisitions 2021',
+    description:
+      'Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.',
+  },
+];
