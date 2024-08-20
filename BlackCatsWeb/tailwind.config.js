eac5792a95a192a759e1ec27c3aed427526b4f4c
zubair-shabir <zubairshabir980@gmail.com>
2024-08-21 02:16:13 +0530

    banner Bug Fix

--- BlackCatsWeb/tailwind.config.js
+++ BlackCatsWeb/tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts}", "./node_modules/flowbite/**/*.js"],
+  content: ["./src/**/*.{html,ts}"],
   darkMode: "selector",
   theme: {
     extend: {},
   },
-  plugins: [require("flowbite/plugin")],
+  plugins: [require('daisyui')],
 };
