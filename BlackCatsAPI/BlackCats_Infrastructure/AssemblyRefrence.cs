2887597cfbce1d85d04abd1f7b79ee10534b9ae8
Hidayat Bhatt <hidayatbhatt@gmail.com>
2024-09-15 19:48:29 +0530

    folder changed

--- BlackCatsAPI/BlackCats_Infrastructure/AssemblyRefrence.cs
+++ BlackCatsAPI/BlackCats_Infrastructure/AssemblyRefrence.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using BlackCats_Application.Abstraction.IService;
+using BlackCats_Application.Services;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BlackCats_Infrastructure
@@ -8,6 +10,8 @@ namespace BlackCats_Infrastructure
 
         public static IServiceCollection AddInfraStructureServices(this IServiceCollection services,IConfiguration configuration)
         {
+            services.AddScoped<ITokenService, TokenService>();
+
             return services;
         }
 
