268edd7e69e4cd333feff0e9da57d2ab8fd0c078
himayatb3 <himayatb3@gmail.com>
2024-09-16 12:40:41 +0530

    sweetAlert

--- BlackCatsWeb/src/environments/environment.ts
+++ BlackCatsWeb/src/environments/environment.ts
@@ -1,4 +1,37 @@
+import Swal from 'sweetalert2'
 export const environment = {
   production: false,
   baseURL: 'http://localhost:5002/api/',
+
+  fireSuccessSwal: (message: string) => {
+    Swal.fire({
+      position: 'top-end',
+      icon: 'success',
+      title: message,
+      showConfirmButton: false,
+      timer: 2000,
+    });
+  },
+
+  fireErrorSwal: (message: string) => {
+    Swal.fire({
+      position: 'top-end',
+      icon: 'error',
+      title: message,
+      showConfirmButton: false,
+      timer: 2000,
+    });
+  },
+
+  fireConfirmSwal: (message: string) => {
+    return Swal.fire({
+      title: message,
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes',
+    });
+  },
 };
