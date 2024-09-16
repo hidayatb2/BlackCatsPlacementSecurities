28fedab9f417805fabe50deaaccdc746048980be
himayatb3 <himayatb3@gmail.com>
2024-09-16 12:56:13 +0530

    updated sweetalert

---
+++ BlackCatsWeb/src/app/Services/toaster-servie.ts
@@ -0,0 +1,34 @@
+import Swal from 'sweetalert2';
+export class ToasterServie {
+  fireSuccessSwal(message: string) {
+    Swal.fire({
+      position: 'top-end',
+      icon: 'success',
+      title: message,
+      showConfirmButton: false,
+      timer: 2000,
+    });
+  }
+
+  fireErrorSwal(message: string) {
+    Swal.fire({
+      position: 'top-end',
+      icon: 'error',
+      title: message,
+      showConfirmButton: false,
+      timer: 2000,
+    });
+  }
+
+  fireConfirmSwal(message: string) {
+    return Swal.fire({
+      title: message,
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes',
+    });
+  }
+}
